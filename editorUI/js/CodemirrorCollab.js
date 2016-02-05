cf12f161467fba03e3b17eefc891c5c463936083
Asa Rudick <Shabonkerz@gmail.com>
2016-02-05 10:43:13 -0600

    display cursor for each connected user

--- editorUI/js/CodemirrorCollab.js
+++ editorUI/js/CodemirrorCollab.js
@@ -2,46 +2,89 @@ import angular from 'angular';
 
 
 let module = angular.module('CodemirrorCollab', [
-  'ui.codemirror'
+	'ui.codemirror'
 ]);
 
-module.directive('codemirrorCollab', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'templates/codemirrorCollab.html',
-    scope: {},
-    link: function(scope, elem, attrs) {
-    },
-    controller: function($scope) {
-      $scope.editorContents = '// the contents\nvar x = function(){\n  console.log(arguments)\n}\n';
+var socket = io.connect(window.location.origin);
+var cursors = {};
 
-      // debugger
+socket.on('news', function (data) {
+	console.log(data);
+	socket.emit('my other event', {
+		my: 'data'
+	});
+});
+socket.on('cursor.move', function (id, cursor) {
+	console.log(arguments);
 
-      $scope.editorOptions = {
-        // lineWrapping : true,
-        lineNumbers: true,
-        // theme: 'twilight',
-        // readOnly: 'nocursor',
-        mode: 'javascript'
-      };
+	// Cursor already exists. Update position.
+	if (cursors[id])
+	{
+		var node = cursors[id];
 
-      $scope.codemirrorLoaded = function(_editor){
-        // Editor part
-        var _doc = _editor.getDoc();
-        _editor.focus();
+		// Get coords from line and character position.
+		var coords = editor.doc.cm.charCoords(cursor, 'local');
 
-        // Options
-        _editor.setOption('firstLineNumber', 10);
-        _doc.markClean();
+		node.style.left = coords.left + "px";
+		node.style.top = coords.top + "px";
+	}
+	// Cursor does not exist. Add new widget.
+	else {
 
-        // Events
-        _editor.on('beforeChange', function(){
-          console.log('beforeChange', arguments);
-        });
-        _editor.on('change', function(){
-          console.log('change', arguments);
-        });
-      };
-    }
-  };
+		var node = document.createElement('div');
+		node.innerHTML = '';
+		node.classList.add('my-widget');
+
+		editor.addWidget(cursor, node, true);
+
+		cursors[id] = node;
+	}
+});
+
+var editor;
+
+module.directive('codemirrorCollab', function () {
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/codemirrorCollab.html',
+		scope: {},
+		link: function (scope, elem, attrs) {},
+		controller: function ($scope) {
+			$scope.editorContents = '// the contents\nvar x = function(){\n  console.log(arguments)\n}\n';
+
+			// debugger
+
+			$scope.editorOptions = {
+				// lineWrapping : true,
+				lineNumbers: true,
+				// theme: 'twilight',
+				// readOnly: 'nocursor',
+				mode: 'javascript'
+			};
+
+			$scope.codemirrorLoaded = function (_editor) {
+				editor = _editor;
+				// Editor part
+				var _doc = editor.getDoc();
+				editor.focus();
+
+				// Options
+				editor.setOption('firstLineNumber', 10);
+				_doc.markClean();
+
+				// Events
+				editor.on('beforeChange', function () {
+					console.log('beforeChange', arguments);
+				});
+				editor.on('change', function () {
+					console.log('change', arguments);
+				});
+				editor.on('cursorActivity', (codemirror) => {
+
+					socket.emit('cursor.move', codemirror.doc.getCursor());
+				});
+
+			};
+		}
+	};
 });
--- server/index.js
+++ server/index.js
@@ -1,25 +1,57 @@
-var express = require('express');
+'use strict';
 
+var express = require('express');
 var routes = require('./routes');
 
 
 var PORT = 8080;
 
+var cursor = {
+	position: {
+		line: 0,
+		ch: 0
+	}
+};
+var _cursors = [];
 
-var main = function() {
-  var app = express();
+var main = function () {
+	var app = require('express')();
+	var server = app.listen(PORT);
+	var io = require('socket.io').listen(server);
 
-  routes.addRoutes(app);
+	io.on('connection', function (socket) {
 
-  app.listen(PORT, function(err) {
-    if (err) {
-     console.log(err);
-    } else {
-     console.log('App started on port', PORT);
-    }
-  });
+		socket.cursor = {
+			line: 0,
+			ch: 0
+		};
+
+		socket.emit('news', {
+			hello: 'world'
+		});
+		socket.on('my other event', function (data) {
+			console.log(data);
+		});
+		// When client moves cursor, update.
+		socket.on('cursor.move', function (position) {
+			socket.cursor = position;
+			var sockets = io.sockets.sockets;
+			
+			Object.keys(io.sockets.connected).forEach(function (k) {
+				if (socket.id === k)
+				{
+					return;
+				}
+
+				var otherSocket = io.sockets.connected[k];
+				otherSocket.emit('cursor.move', socket.id, socket.cursor);
+			});
+		});
+	});
+
+	routes.addRoutes(app);
 };
 
 if (require.main === module) {
-  main();
+	main();
 }
